fa488befde7fc8a61e5ce9db0685129b651c07ee
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2020-06-01 14:40:04 -0600

    Update app-explorer.sh

--- app/app-explorer.sh
+++ app/app-explorer.sh
@@ -9,7 +9,7 @@ app_install_explorer()
     heading "Installing Explorer to '$EXPLORER_PATH'..."
 
     rm -rf "$EXPLORER_PATH"
-    git clone -b origin https://github.com/Plusid/core-explorer.git "$EXPLORER_PATH" && cd "$EXPLORER_PATH"
+    git clone -b develop https://github.com/Plusid/core-explorer.git "$EXPLORER_PATH" && cd "$EXPLORER_PATH"
     yarn
     yarn add connect-history-api-fallback express
 
