b0a53e8390e15723d19369cb9cf81fe1dce809ca
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2020-11-14 16:42:57 +0400

    Update app-core.sh

--- app/app-core.sh
+++ app/app-core.sh
@@ -294,8 +294,8 @@ app_install_core()
             git config url."git@github.com:".insteadOf "https://github.com/"
         fi
         echo "start git config"
-        git config --global user.email "spe"
-        git config --global user.name "Plusi"
+        git config --global user.email "speed-eau@hotmail.fr"
+        git config --global user.name "Plusid"
         echo "end git config"
         git checkout -b chore/bridgechain-changes
         if [[ "$GIT_CORE_ORIGIN" != "" ]]; then
